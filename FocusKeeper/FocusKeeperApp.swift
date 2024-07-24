148f90f78344987df67ec7c1f2be6b2ffe53a3d1
101itsGabe <manngabe@gmail.com>
2024-07-24 18:39:13 -0500

    Initial Commit

---
+++ FocusKeeper/FocusKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusKeeperApp.swift
+//  FocusKeeper
+//
+//  Created by Gabriel Mannheimer on 7/24/24.
+//
+
+import SwiftUI
+
+@main
+struct FocusKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
